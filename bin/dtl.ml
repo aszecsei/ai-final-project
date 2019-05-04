f66fedcc4f283802bc9b24a837c95f4cd4aef18f
willem-dejong <cswdejong@gmail.com>
2019-05-04 14:26:24 -0500

    more dtl

--- bin/dtl.ml
+++ bin/dtl.ml
@@ -2,14 +2,19 @@ open Lib.Reader
 open Lib.Types
 open Lib.Creator
 open Lib.Decision_tree_j
+open Yojson.Safe
 (*
- * first argument is the type of data (Balance | Mushrooms | TicTacToe | #$#$#$##$#$)
+ * first argument is the type of data (Balance | Mushrooms | TicTacToe | Car) case insensitive
  * second argument is the file path for the file that contains the data in CSV format
+ * third argument is the file path for where to spit the json output
  *)
 let () =
-        if Array.length Sys.argv <> 3 then 
+        let len = Array.length Sys.argv in
+        if len < 3 then 
                 failwith "Wrong parameters"
         else (
+                let doPretty   = (len>3)&&(String.contains Sys.argv.(3) 'p') in
+                let doWrite    = (len>3)&&(String.contains Sys.argv.(3) 'w') in
                 let kind       = getDataType Sys.argv.(1) in
                 let cci        = getCCI kind in
                 let classes    = getClassifications kind in 
@@ -25,9 +30,22 @@ let () =
                         Printf.printf "\n";
                 in
                 let tree = decision_tree_learning examples attributes [||] classes in
+                let tree_str = 
+                        if doPretty then (pretty_to_string (from_string (string_of_decision_tree tree))) 
+                        else (string_of_decision_tree tree)
+                in
+                let writeOut str =
+                        if (len<5) then failwith "need to provide the file path for out put with write option"
+                        else (
+                                let outf = open_out Sys.argv.(4) in
+                                Printf.fprintf outf "%s" str;
+                                close_out outf;
+                             )
+                in
                 print_example_array (Array.sub examples 0 10); (*FOR DEBUG PURPOSES*)
                 print_attribute_array attributes; (*FOR DEBUG PURPOSES*)
                 temp classes; (*FOR DEBUG PURPOSES*)
-                Printf.printf "%s\n" (string_of_decision_tree tree) 
-             )
+                Printf.printf "%s\n" tree_str;
+                if doWrite then writeOut tree_str else ();
+            )
 ;;
