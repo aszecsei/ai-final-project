c01d1dfef3d9213405737b222f229f9604a380eb
willem-dejong <cswdejong@gmail.com>
2019-05-03 20:29:57 -0500

    added ability to read in without classification

--- lib/reader.ml
+++ lib/reader.ml
@@ -35,14 +35,13 @@ let create_example_from_list (l:string list) (cci:int) : example=
         let len = List.length l in
         match cci<len with
         | true  -> (build_ex l 0 [])
-        | false -> failwith "oops create_example_list failed"
+        | false -> (build_ex (l@[""]) len []) 
 ;;
 
 (*print_example (create_example_from_list ["1";"3";"5";"7"] 2)*)
 
 let create_example_from_str (s:string) (cci:int) : example=
         create_example_from_list (String.split_on_char ',' s) cci
-       
 ;;
 
 (*print_example (create_example_from_str "1,3,5,7" 2)*)
@@ -59,4 +58,4 @@ let read_gen (file:string) (cci:int) : example array=
                 close_in inf;
                 Array.of_list (List.rev !exs)
 ;;
-(*print_example_array (read_gen "test.txt" 0);;*)
+(*print_example_array (read_gen "test.txt" 10);;*)
