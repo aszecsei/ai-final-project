a6cb30aff4a02715a15957f6c82185757e1f864a
Diego Castaneda <diego-castaneda@uiowa.edu>
2019-05-06 11:25:43 -0500

    fix reader

--- lib/reader.ml
+++ lib/reader.ml
@@ -2,19 +2,19 @@ open Types
 (*type example={attributes:string list; value:string}*)
 
 let create_example_from_list (l:string list) (cci:int) : example=
-        let rec build_ex (l:string list) (ind:int) (attr:string list) : example=
-                match l, ind=cci with
+        let rec build_ex (l:string list) (ind:int) (attr:string list) (icci:int) : example=
+                match l, ind=icci with
                 | h::t, true -> {attributes = (attr@t); value=h}
-                | h::t, false ->(build_ex t (ind+1) (attr@[h]))
-                | _,_ -> failwith "oops build_ex failed"
+                | h::t, false ->(build_ex t (ind+1) (attr@[h]) icci)
+                | _,_ -> Printf.printf "%s\n" (string_of_int icci); failwith "oops build_ex failed"
         in
         let len = List.length l in
         match cci<len with
-        | true  -> (build_ex l 0 [])
-        | false -> (build_ex (l@[""]) len []) 
+        | true  -> (build_ex l 0 [] cci)
+        | false -> (build_ex (l@[""]) 0 [] len) 
 ;;
 
-(*print_example (create_example_from_list ["1";"3";"5";"7"] 2)*)
+(*let () = print_example (create_example_from_list ["1";"3";"5";"7"] 100);;*)
 
 let create_example_from_str (s:string) (cci:int) : example=
         create_example_from_list (String.split_on_char ',' s) cci
