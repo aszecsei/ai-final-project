1340b5ed93b78217a1607537d783ab71f641a899
willem-dejong <cswdejong@gmail.com>
2019-05-09 18:28:04 -0500

    model selection ocaml

--- scripts/modelselectscript.py
+++ scripts/modelselectscript.py
@@ -5,24 +5,24 @@ import graphmaker
 filename= "output/ModelSelection.csv"
 subprocess.run(["rm","-f",filename])
 
-for i in range(1,101):
+for i in range(1,2):
     print("running car "+str(i))
     subprocess.run(["dune", "exec", "bin/modelSelect.exe", "--", "-q", "-g", filename, "CAR", "data/cars/car.data"])
 
-for i in range(1,101):
+for i in range(1,2):
     print("running mushrooms "+str(i))
     subprocess.run(["dune", "exec", "bin/modelSelect.exe", "--", "-q", "-g", filename, "MUSHROOMS", "data/mushrooms/agaricus-lepiota.data"])
 
-for i in range(1,101):
+for i in range(1,2):
     print("running tictoctoe "+str(i))
     subprocess.run(["dune", "exec", "bin/modelSelect.exe", "--", "-q", "-g", filename, "TICTACTOE", "data/ttt/tic-tac-toe.data"])
 
-for i in range(1,101):
+for i in range(1,2):
     print("running balance "+str(i))
     subprocess.run(["dune", "exec", "bin/modelSelect.exe", "--", "-q", "-g", filename, "BALANCE", "data/balance/balance-scale.data"])
 
 data = pd.read_csv(
-    './data.csv', names=['DataSource', 'Algorithm', 'MaxDepth', 'ErrT', 'ErrV'])
+    filename, names=['DataSource', 'Algorithm', 'MaxDepth', 'ErrT', 'ErrV'])
 
 graphmaker.generate_errt_vs_errv_chart(data, 'BALANCE', 'Balance', 'OURS','_ocaml')
 graphmaker.generate_errt_vs_errv_chart(data, 'TICTACTOE', 'Tic-Tac-Toe', 'OURS','_ocaml')
